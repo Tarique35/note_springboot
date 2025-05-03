5692ad4cb58f2f8f32509b94f06aa3afc9f3bd97
Tarique35 <ansaritarique35@gmail.com>
2025-05-03 15:17:40 +0530

    tarique

--- src/main/java/com/note/application/config/CorsConfig.java
+++ src/main/java/com/note/application/config/CorsConfig.java
@@ -6,20 +6,20 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@Configuration
+//@Configuration
 public class CorsConfig {
 
-	@Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.addAllowedOriginPattern("*");// Allow all origins
-		corsConfiguration.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, etc.)
-		corsConfiguration.addAllowedHeader("*");// Allow all headers
-		corsConfiguration.setAllowCredentials(true);// Allow cookies and credentials
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfiguration);// Apply this to all endpoints
-
-		return new CorsFilter(source);
-	}
+//	@Bean
+//	public CorsFilter corsFilter() {
+//		CorsConfiguration corsConfiguration = new CorsConfiguration();
+//		corsConfiguration.addAllowedOriginPattern("*");// Allow all origins
+//		corsConfiguration.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, etc.)
+//		corsConfiguration.addAllowedHeader("*");// Allow all headers
+//		corsConfiguration.setAllowCredentials(true);// Allow cookies and credentials
+//
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", corsConfiguration);// Apply this to all endpoints
+//
+//		return new CorsFilter(source);
+//	}
 }
--- src/main/java/com/note/application/config/SecurityConfig.java
+++ src/main/java/com/note/application/config/SecurityConfig.java
@@ -11,12 +11,17 @@ import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.web.SecurityFilterChain;
 //import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Configuration
 //@EnableWebSecurity
 public class SecurityConfig {
 
 //	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//		return http.csrf(csrf -> csrf.disable())
+//				.authorizeHttpRequests(auth -> auth.requestMatchers("/all/notes", "/login", "/register").permitAll().anyRequest()
+//						.authenticated())
+//				.httpBasic(Customizer.withDefaults())
+//				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
 //	}
 }
